4a798246c7126c25cce9bfab01a5bc0cfade5a73
David Waring <david.waring2@bbc.co.uk>
2024-08-16 12:07:03 +0100

    Allow apps and other modules to add extra configuration files/sections.

--- python/lib/rt_m1_client/configuration.py
+++ python/lib/rt_m1_client/configuration.py
@@ -28,7 +28,7 @@ import configparser
 import os
 import os.path
 
-from typing import List
+from typing import List, Optional, Iterable
 
 class Configuration:
     '''Application configuration container
@@ -40,7 +40,7 @@ class Configuration:
     log_dir = /var/log/rt-5gms
     state_dir = /var/cache/rt-5gms
     run_dir = /run/rt-5gms
-
+    
     [m1-client]
     log_level = info
     data_store = %(state_dir)s/m1-client
@@ -62,14 +62,24 @@ class Configuration:
             self.__config_filename = os.path.expanduser(os.path.join('~', '.rt-5gms', 'm1-client.conf'))
         else:
             self.__config_filename = os.path.join(os.path.sep, 'etc', 'rt-5gms', 'm1-client.conf')
-        self.__default_config = configparser.ConfigParser()
-        self.__default_config.read_string(self.DEFAULT_CONFIG)
-        self.__config = configparser.ConfigParser()
-        self.__config.read_string(self.DEFAULT_CONFIG)
-        if os.path.exists(self.__config_filename):
-            self.__config.read(self.__config_filename)
+        self.__default_section = None
+        self.__default_config = {}
+        self.__config = {}
+        self.__config_filepath = {}
+        self.addSection('m1-client', self.DEFAULT_CONFIG, self.__config_filename, make_default=True)
+        
+    def addSection(self, section_name: str, defaults: str, config_filepath: Optional[str] = None, *, make_default: bool = False):
+        if make_default:
+            self.__default_section = section_name
+        self.__default_config[section_name] = configparser.ConfigParser()
+        self.__default_config[section_name].read_string(defaults)
+        self.__config[section_name] = configparser.ConfigParser()
+        self.__config[section_name].read_string(defaults)
+        self.__config_filepath[section_name] = config_filepath
+        if config_filepath is not None and os.path.exists(config_filepath):
+            self.__config[section_name].read(config_filepath)
 
-    def isKey(self, key: str) -> str:
+    def isKey(self, key: str, section: Optional[str] = None) -> str:
         '''Does a configuration field key exist?
 
         This tests *key* for being a valid configuration option field key name.
@@ -77,11 +87,13 @@ class Configuration:
         :returns: The key string if it is a valid configuration field key.
         :raises: ValueError if the key string does not match a known configuration field key.
         '''
-        if key in self.__default_config['m1-client']:
+        if section is None:
+            section = self.__default_section
+        if key in self.__default_config[section]:
             return key
         raise ValueError('Not a valid configuration option')
 
-    def get(self, key: str, default: str = None, raw: bool = False) -> str:
+    def get(self, key: str, default: str = None, raw: bool = False, section: Optional[str] = None) -> str:
         '''Get a configuration value
 
         Retrieves the value for configuration option *key*. If the *key* does not exist the *default* will be returned. If *raw* is
@@ -89,9 +101,11 @@ class Configuration:
 
         :returns: The configuration option *key* value or *default* if key does not exist.
         '''
-        return self.__config.get('m1-client', key, raw=raw, fallback=default)
+        if section is None:
+            section = self.__default_section
+        return self.__config.get(section, key, raw=raw, fallback=default)
 
-    def set(self, key: str, value: str) -> bool:
+    def set(self, key: str, value: str, section: Optional[str] = None) -> bool:
         '''Set a configuration value
 
         Sets the raw *value* for configuration option *key*. If *key* is not a valid configuration option then ValueError exception
@@ -99,59 +113,82 @@ class Configuration:
 
         The configuration is saved once the *key* option has been set.
         '''
-        self.isKey(key)
-        if key in self.__default_config['DEFAULT']:
-            section = 'DEFAULT'
+        if section is None:
+            section = self.__default_section
+        self.isKey(key, section=section)
+        if key in self.__default_config[section]['DEFAULT']:
+            cfg_section = 'DEFAULT'
         else:
-            section = 'm1-client'
-        self.__config.set(section, key, value)
+            cfg_section = section
+        self.__config[section].set(cfg_section, key, value)
         self.__saveConfig()
         return True
 
-    def isDefault(self, key: str) -> bool:
+    def isDefault(self, key: str, section: Optional[str] = None) -> bool:
         '''Checks if a key contains the default configuration value
 
         :returns: ``True`` if the configuration value for *key* is the default value, or ``False`` otherwise.
         '''
-        return self.__config.get('m1-client', key) == self.__default_config.get('m1-client', key)
+        if section is None:
+            section = self.__default_section
+        return self.__config[section].get(section, key) == self.__default_config[section].get(section, key)
 
-    def getKeys(self) -> List[str]:
+    def getKeys(self, section: Optional[str] = None) -> List[str]:
         '''Get a list of configuration field name keys
 
         :returns: A list of configuration key names.
         '''
-        return list(self.__default_config['m1-client'].keys())
+        if section is None:
+            section = self.__default_section
+        return list(self.__default_config[section][section].keys())
 
-    def resetValue(self, key: str) -> bool:
+    def resetValue(self, key: str, section: Optional[str] = None) -> bool:
         '''Reset a configuration field to its default value
 
         :returns: ``True`` if the field was reset or ``False`` if the field already contained the default value.
         '''
-        if self.isDefault(key):
+        if section is None:
+            section = self.__default_section
+        if self.isDefault(key, section=section):
             return False
-        return self.set(key, self.__default_config.get('m1-client', key))
+        return self.set(key, self.__default_config[section].get(section, key), section=section)
 
-    def __saveConfig(self):
-        '''Save the current configuration to local storage
+    def __saveConfig(self, sections: Optional[Iterable[str]] = None):
+        '''Save the configurations for the given sections to local storage
+
+        :meta private-method:
+
+        If `sections` is None then all sections with a configuration filename attached will be saved.
+        '''
+
+        if sections is None:
+            sections = self.__config_filepath.keys()
+        for s in sections:
+            self.__saveConfigSection(s)
+
+    def __saveConfigSection(self, section: Optional[str]):
+        '''Save the current configuration for a given section to local storage
 
         :meta private-method:
 
         Will save the current configuration to the relevant local file. Fields with the default value will be saved as a comment.
         '''
-        cfgdir = os.path.dirname(self.__config_filename)
+        if section not in self.__config_filepath or self.__config_filepath[section] is None:
+            return
+        cfgdir = os.path.dirname(self.__config_filepath[section])
         if not os.path.exists(cfgdir):
             old_umask = os.umask(0)
             try:
                 os.makedirs(cfgdir, mode=0o755)
             finally:
                 os.umask(old_umask)
-        with open(self.__config_filename, 'w') as cfgout:
-            for section in ['DEFAULT'] + self.__config.sections():
+        with open(self.__config_filepath[section], 'w') as cfgout:
+            for sect in ['DEFAULT'] + self.__config[section].sections():
                 cfgout.write(f'[{section}]\n')
-                for key in self.__config[section]:
-                    cfgvalue = self.__config.get(section, key, raw=True)
-                    defvalue = self.__default_config.get(section, key, raw=True)
-                    if (section == 'DEFAULT' or key not in self.__config['DEFAULT']):
+                for key in self.__config[section][sect]:
+                    cfgvalue = self.__config[section].get(sect, key, raw=True)
+                    defvalue = self.__default_config[section].get(sect, key, raw=True)
+                    if (sect == 'DEFAULT' or key not in self.__config[section]['DEFAULT']):
                         if cfgvalue == defvalue:
                             cfgout.write('#')
                         cfgout.write(f'{key} = {cfgvalue}\n')
@@ -163,7 +200,10 @@ class Configuration:
         :returns: A ``str`` representing the configuration.
         '''
         buf = StringIO()
-        self.__config.write(buf)
+        for sect,config in self.__config.items():
+            buf.write(f'#=============== {sect} ================#\n')
+            config.write(buf)
+            buf.write('\n')
         return buf.getvalue()
 
     def __repr__(self):
