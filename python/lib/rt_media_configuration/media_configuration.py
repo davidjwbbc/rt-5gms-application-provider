ab607721272b4bb475716b98471f7b1649346bb5
David Waring <david.waring2@bbc.co.uk>
2024-08-19 17:38:34 +0100

    Add model deltas system (these can be viewed, approved and applied)

--- python/lib/rt_media_configuration/media_configuration.py
+++ python/lib/rt_media_configuration/media_configuration.py
@@ -34,7 +34,8 @@ rt_m1_client.M1Session object to synchronise that configuration with the
 import configparser
 import json
 import logging
-from typing import Optional, Iterable, Dict
+from typing import Optional, Iterable, Dict, List
+import uuid
 
 import aiofiles
 
@@ -42,7 +43,8 @@ from rt_m1_client.configuration import Configuration
 from rt_m1_client.session import M1Session
 from rt_m1_client.data_store import DataStore
 
-from .media_entry import MediaEntry
+from .media_session import MediaSession
+from .delta_operations import *
 
 DEFAULT_CONFIG = '''[media-configuration]
 m5_authority = example.com:7777
@@ -58,13 +60,15 @@ logic to use the rt_m1_client.M1Session object to synchronise that
 configuration with the 5GMS AF.
 '''
 
-    def __init__(self, configfile: Optional[str] = None, persistent_data_store: Optional[DataStore] = None):
+    def __init__(self, configfile: Optional[str] = None, asp_id: Optional[str] = None,
+                 persistent_data_store: Optional[DataStore] = None):
         self.__outputs = []
-        self.__model = {"media": {}}
+        self.__model = {"sessions": {}, "aspId": None}
+        self.asp_id = asp_id
         self.__extraConfigFile = configfile
         self.__config = Configuration()
         self.__config.addSection('media-configuration', DEFAULT_CONFIG, self.__extraConfigFile)
-        self.__data_store_dir = persistent_data_store
+        self.__data_store = persistent_data_store
         self.__log = logging.getLogger(__name__ + '.' + self.__class__.__name__)
         self.__m1_session = None
 
@@ -74,13 +78,28 @@ configuration with the 5GMS AF.
         return self.__asyncInit().__await__()
 
     def __repr__(self) -> str:
-        return f'{__name__}.{self.__class__.__name__}({self.__extraConfigFile!r}, persistent_data_store={self.__data_store_dir!r})'
+        ret = f'{__name__}.{self.__class__.__name__}({self.__extraConfigFile!r}'
+        if self.__model['aspId'] is not None:
+            ret += f', asp_id={self.__model["aspId"]!r}'
+        if self.__data_store is not None:
+            ret += f', persistent_data_store={self.__data_store!r}'
+        ret += ')'
+        return ret
 
     def __str__(self) -> str:
-        return json.dumps(self.__model["media"], default=MediaConfiguration.jsonObjectHandler, sort_keys=True, indent=2)
+        return self.serialise(pretty=True)
+
+    def serialise(self, pretty: bool = False) -> str:
+        obj = {'streams': self.__model["sessions"]}
+        kwargs = {}
+        if pretty:
+            kwargs = {'sort_keys': True, 'indent': 2}
+        if self.__model['aspId'] is not None:
+            obj['aspId'] = self.__model['aspId']
+        return json.dumps(obj, default=MediaConfiguration.jsonObjectHandler, **kwargs)
 
     async def reset(self) -> None:
-        self.__model = {"media": {}}
+        self.__model["sessions"] = {}
 
     async def restoreModel(self) -> bool:
         '''Restore the model to the last known configuration.
@@ -99,44 +118,46 @@ configuration with the 5GMS AF.
             return False
         return True
 
-    async def newMediaEntry(self, stream_id: str, *args, **kwargs) -> MediaEntry:
-        '''Create a new MediaEntry and attach it to this MediaConfiguration
+    async def newMediaSession(self, *args, **kwargs) -> MediaSession:
+        '''Create a new MediaSession and attach it to this MediaConfiguration
 
-        :see: MediaEntry constructor for parameters.
-        :return: A new MediaEntry object attached to this MediaConfiguration.
+        :see: MediaSession constructor for parameters.
+        :return: A new MediaSession object attached to this MediaConfiguration.
         '''
-        entry = await MediaEntry(*args, **kwargs)
-        entry.id = stream_id
-        self.__model["media"][stream_id] = entry
+        entry = await MediaSession(*args, **kwargs)
+        if entry.id is None:
+            entry.id = str(uuid.uuid4())
+        self.__model["sessions"][entry.id] = entry
         return entry
         
-    async def addMediaEntry(self, stream_id: str, entry: MediaEntry) -> bool:
-        entry.id = stream_id
-        self.__model["media"][stream_id] = entry
+    async def addMediaSession(self, session: MediaSession) -> bool:
+        if session.id is None:
+            session.id = str(uuid.uuid4())
+        self.__model["sessions"][session.id] = session
         return True
 
-    async def removeMediaEntry(self, *, stream_id: Optional[str] = None, entry: Optional[MediaEntry] = None) -> bool:
-        if stream_id is None and entry is None:
+    async def removeMediaEntry(self, *, session_id: Optional[str] = None, entry: Optional[MediaSession] = None) -> bool:
+        if session_id is None and entry is None:
             return False
-        if stream_id is not None:
+        if session_id is not None:
             if entry is not None:
-                raise RuntimeError('MediaConfiguration.removeMediaEntry takes either stream_id or entry, not both')
-            if stream_id not in self.__model["media"]:
+                raise RuntimeError('MediaConfiguration.removeMediaEntry takes either session_id or entry, not both')
+            if session_id not in self.__model["sessions"]:
                 return False
-            del self.__model["media"][stream_id]
+            del self.__model["sessions"][session_id]
             return True
         else:
-            for k,v in self.__model["media"]:
+            for k,v in self.__model["sessions"]:
                 if v == entry:
-                    del self.__model["media"][k]
+                    del self.__model["sessions"][k]
                     return True
         return False
 
-    async def mediaEntries(self) -> Iterable[MediaEntry]:
-        return self.__model["media"].values()
+    async def mediaSessions(self) -> Iterable[MediaSession]:
+        return self.__model["sessions"].values()
 
-    async def mediaEntryById(self, stream_id: str) -> Optional[MediaEntry]:
-        return self.__model["media"].get(stream_id, None)
+    async def mediaSessionById(self, session_id: str) -> Optional[MediaSession]:
+        return self.__model["sessions"].get(session_id, None)
 
     async def synchronise(self):
         '''Synchronise MediaConfiguration
@@ -147,11 +168,71 @@ configuration with the 5GMS AF.
         out the DataStore (as configured in the constructor). The M8 outputs will be triggered to republish any M8 objects.
         '''
         if self.__m1_session is None:
-            self.__m1_session = await M1Session(host_address=(self.__config.get('m1_address'),self.__config.get('m1_port')),
-                                                persistent_data_store=self.__data_store_dir,
-                                                certificate_signer=self.__config.get('certificate_signing_class'))
-        raise Exception("not implemenetd yet")
-    
+            m1_authority = (self.__config.get('m1_address'),self.__config.get('m1_port'))
+            cert_signer = self.__config.get('certificate_signing_class')
+            self.__m1_session = await M1Session(host_address=m1_authority, persistent_data_store=self.__data_store,
+                                                certificate_signer=cert_signer)
+        af_mc = await MediaConfiguration(self.__extraConfigFile, asp_id=self.__model['aspId'], persistent_data_store=self.__data_store)
+        af_imp = await M1SessionImporter(authority=m1_authority, persistent_data_store=self.__data_store, certificate_signer=cert_signer)
+        await af_imp.import_to(af_mc)
+        deltas = await af_mc.deltas(self)
+        print(repr(deltas))
+        raise Exception("not implemented yet")
+
+    async def deltas(self, other: "MediaConfiguration") -> List[DeltaOperation]:
+        '''Get the list of operations needed to change this configuration into the configuration in other
+        '''
+        ret = []
+        # Check each session I hold to see if it exists in the other configuration or not
+        to_del = []
+        have = []
+        to_add = []
+        for session in self.__model['sessions'].values():
+            for o_session in other.__model['sessions'].values():
+                if session.shallow_eq(o_session):
+                    have += [(session, o_session)]
+                    break
+            else:
+                to_del += [session]
+        for o_session in other.__model['sessions'].values():
+            for session in self.__model['sessions'].values():
+                if session.shallow_eq(o_session):
+                    break
+            else:
+                to_add += [o_session]
+        ret += [MediaSessionDeltaOperation(add=session) for session in to_add]
+        ret += [MediaSessionDeltaOperation(remove=session) for session in to_del]
+        # check sub-structures of sessions we have for changes
+        for session,o_session in have:
+            if session.media_entry is None and o_session.media_entry is not None:
+                ret += [MediaEntryDeltaOperation(session, add=o_session.media_entry)]
+            elif session.media_entry is not None:
+                ret += session.media_entry.deltas(o_session.media_entry, session)
+
+            if session.dynamic_policies is None and o_session.dynamic_policies is not None:
+                ret += [MediaDynamicPolicyDeltaOperation(session, add=dp) for dp in o_session.dynamic_policies]
+            elif session.dynamic_policies is not None and o_session.dynamic_policies is None:
+                ret += [MediaDynamicPolicyDeltaOperation(session, remove=dp) for dp in session.dynamic_policies]
+            elif session.dynamic_policies is not None and o_session.dynamic_policies is not None:
+                # make deltas for individual dynamic policies
+                for dp_id,dp in o_session.dynamic_policies.items():
+                    if dp_id not in session.dynamic_policies:
+                        ret += [MediaDynamicPolicyDeltaOperation(session, add=(dp_id,dp))]
+                    elif dp != session.dynamic_policies[dp_id]:
+                        ret += [MediaDynamicPolicyDeltaOperation(session, modify(dp_id,dp))]
+                for dp_id in session.dynamic_policies.keys():
+                    if dp_id not in o_session.dynamic_policies:
+                        ret += [MediaDynamicPolicyDeltaOperation(session, remove=dp_id)]
+
+            if session.reporting_configurations is None and o_session.reporting_configurations is not None:
+                if o_session.reporting_configurations.consumption is not None:
+                    ret += [MediaConsumptionReportingDeltaOperation(session, add=o_session.reporting_configurations.consumption)]
+                if o_session.reporting_configurations.metrics is not None:
+                    ret += [MediaMetricsReportingDeltaOperation(session, add=mr) for mr in o_session.reporting_configurations.metrics]
+            elif session.reporting_configurations is not None:
+                ret += session.reporting_configurations.deltas(o_session.reporting_configurations, session)
+        return ret
+
     async def __asyncInit(self):
         '''Asynchronous object instantiation
 
@@ -168,5 +249,16 @@ configuration with the 5GMS AF.
         if fn is not None:
             return fn()
         raise TypeError(f'Object of type {obj.__class__.__name__} is not JSON serialisable')
+
+    @property
+    def asp_id(self) -> Optional[str]:
+        return self.__model["aspId"]
+
+    @asp_id.setter
+    def asp_id(self, value: Optional[str]):
+        if value is not None:
+            if not isinstance(value, str) or len(value) == 0:
+                raise TypeError('MediaConfiguration.aspId must be a non-empty str or None')
+        self.__model["aspId"] = value
 
 # vim:ts=8:sts=4:sw=4:expandtab:
