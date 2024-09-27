1aac2d5591e8e629f625b492afc317e93989bedd
David Waring <david.waring2@bbc.co.uk>
2024-09-27 16:05:18 +0100

    Add new classes to rt_media_configuration module default import

--- python/lib/rt_media_configuration/__init__.py
+++ python/lib/rt_media_configuration/__init__.py
@@ -8,6 +8,17 @@ from .media_dynamic_policy import MediaDynamicPolicy
 from .media_dynamic_policy_session_context import MediaDynamicPolicySessionContext
 from .media_reporting_configuration import MediaReportingConfiguration, MediaConsumptionReportingConfiguration, MediaMetricsReportingConfiguration
 from .media_qos_parameters import MediaQoSParameters
+from .media_caching_configuration import MediaCachingConfiguration
+from .media_caching_directive import MediaCachingDirective
+from .media_charging_specification import MediaChargingSpecification
+from .media_dynamic_policy import MediaDynamicPolicy
+from .media_dynamic_policy_session_context import MediaDynamicPolicySessionContext
+from .media_geo_fencing import MediaGeoFencing
+from .media_path_rewrite_rule import MediaPathRewriteRule
+from .media_server_certificate import MediaServerCertificate
+from .media_session import MediaSession
+from .media_supplementary_distribution_network import MediaSupplementaryDistributionNetwork
+from .media_url_signature import MediaURLSignature
 from .gpsi import Gpsi
 from .bitrate import Bitrate
 from .snssai import Snssai
